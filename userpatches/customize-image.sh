dafc7b652722aeb965c7c220cdc6559be3e669c1
ADN ROBOTICS FR <adnroboticsfr@gmail.com>
2025-04-25 18:06:01 +0200

    Update

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# arguments: $RELEASE $LINUXFAMILY $BOARD $BUILD_DESKTOP
+#
+# This is the image customization script
+
+# NOTE: It is copied to /tmp directory inside the image
+# and executed there inside chroot environment
+# so don't reference any files that are not already installed
+
+# NOTE: If you want to transfer files between chroot and host
+# userpatches/overlay directory on host is bind-mounted to /tmp/overlay in chroot
+# The sd card's root path is accessible via $SDCARD variable.
+
+# shellcheck enable=requires-variable-braces
+# shellcheck disable=SC2034
+
+RELEASE=$1
+LINUXFAMILY=$2
+BOARD=$3
+BUILD_DESKTOP=$4
+
 sudo apt update 
 sudo apt upgrade
 git clone https://github.com/ADNroboticsfr/smartpi-gpio.git
@@ -18,4 +38,97 @@ sudo rosdep init
 sudo apt-get install -y python3-dev python3-pip libjpeg-dev zlib1g-dev libtiff-dev
 sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old
 sudo pip3 install face_recognition numpy
-sudo pip3 install --upgrade opencv-python opencv-contrib-python
+sudo 
+
+Main() {
+    case "${BOARD}" in
+        smartpad)
+            rotateConsole
+            rotateScreen
+            rotateTouch
+            disableDPMS
+            if [[ "${BUILD_DESKTOP}" = "yes" ]]; then
+                patchLightdm
+                copyOnboardConf
+                patchOnboardAutostart
+                installScreensaverSetup
+            fi
+            if [[ "${RELEASE}" = "bookworm" ]]; then
+                echo "release bookworm"
+            fi
+            ;;
+    esac
+}
+
+rotateConsole() {
+    local bootcfg="/boot/armbianEnv.txt"
+    echo "Rotate tty console by default ..."
+    echo "extraargs=fbcon=rotate:2" >> "${bootcfg}"
+    echo "Current configuration (${bootcfg}):"
+    cat "${bootcfg}"
+    echo "Rotate tty console by default ... done!"
+}
+
+rotateScreen() {
+    local src="/tmp/overlay/02-smartpad-rotate-screen.conf"
+    local dest="/etc/X11/xorg.conf.d/"
+    echo "Install rotated screen configuration ..."
+    cp -v "${src}" "${dest}"
+    echo "DEBUG:"
+    ls -l "${dest}"
+    echo "Install rotated screen configuration ... [DONE]"
+}
+
+rotateTouch() {
+    local src="/tmp/overlay/03-smartpad-rotate-touch.conf"
+    local dest="/etc/X11/xorg.conf.d/"
+    echo "Install rotated touch configuration ..."
+    cp -v "${src}" "${dest}"
+    echo "DEBUG:"
+    ls -l "${dest}"
+    echo "Install rotated touch configuration ... [DONE]"
+}
+
+disableDPMS() {
+    local src="/tmp/overlay/04-smartpad-disable-dpms.conf"
+    local dest="/etc/X11/xorg.conf.d/"
+    echo "Disable DPMS power management ..."
+    cp -v "${src}" "${dest}"
+    echo "DEBUG:"
+    ls -l "${dest}"
+    echo "Disable DPMS power management ... [DONE]"
+}
+
+patchLightdm() {
+    local conf="/etc/lightdm/lightdm.conf.d/12-onboard.conf"
+    echo "Enable OnScreen Keyboard in Lightdm ..."
+    echo "onscreen-keyboard = true" | tee "${conf}"
+    echo "Enable OnScreen Keyboard in Lightdm ... [DONE]"
+}
+
+copyOnboardConf() {
+    echo "Copy onboard default configuration ..."
+    mkdir -p /etc/onboard
+    cp -v /tmp/overlay/onboard-defaults.conf /etc/onboard/
+    echo "Copy onboard default configuration ... [DONE]"
+}
+
+patchOnboardAutostart() {
+    local conf="/etc/xdg/autostart/onboard-autostart.desktop"
+    echo "Patch Onboard Autostart file ..."
+    sed -i '/OnlyShowIn/s/^/# /' "${conf}"
+    echo "Patch Onboard Autostart file ... [DONE]"
+}
+
+installScreensaverSetup() {
+    local src="/tmp/overlay/skel-xscreensaver"
+    local dest="/etc/skel/.xscreensaver"
+    echo "Install screensaver configuration ..."
+    \cp -fv "${src}" "${dest}"
+    echo "DEBUG:"
+    ls -al "$(dirname "${dest}")"
+    echo "Install screensaver configuration ... [DONE]"
+}
+
+
+Main "$@"
